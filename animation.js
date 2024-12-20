cb2b45c1ef9273e6b3e4c529134f989533b3e667
Divya Singh <divyasingh.300246@gmail.com>
2024-12-20 20:32:16 +0530

    commit

--- animation.js
+++ animation.js
@@ -4,7 +4,7 @@
  *  .  .   change these values:   .  *
    .      * .        .          * .       */
 const STAR_COLOR = '#fff';
-const STAR_SIZE = 3;
+const STAR_SIZE = 4;
 const STAR_MIN_SCALE = 0.2;
 const OVERFLOW_THRESHOLD = 50;
 const STAR_COUNT = ( window.innerWidth + window.innerHeight ) / 8;
